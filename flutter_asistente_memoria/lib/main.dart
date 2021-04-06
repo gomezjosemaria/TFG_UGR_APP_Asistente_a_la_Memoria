264ef02b2a896e5c9d15ad9ff08ef4eb0e7f5e7a
José Mª Gómez García <33324337+gomezjosemaria@users.noreply.github.com>
2021-04-06 21:51:34 +0200

    Corrección de import

--- flutter_asistente_memoria/lib/main.dart
+++ flutter_asistente_memoria/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/chemi/OneDrive/Escritorio/DGPGIT/TFG_UGR_APP_Asistente_a_la_Memoria/flutter_asistente_memoria/lib/screens/choose_login/choose_login.dart';
+import 'package:flutter_asistente_memoria/screens/choose_login/choose_login.dart';
 
 void main() {
   runApp(MyApp());
